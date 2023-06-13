5e79dbafe42b79337fe35481db8baf352fdb5bca
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2023-06-13 17:27:21 +0530

    Update fetchFromAPI.jsx

--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -10,8 +10,8 @@ const options = {
 
     },
     headers: {
-      'X-RapidAPI-Key': 'a0569ee998mshff72b6e48e35b2cp1c3512jsn2b6602d51d68',//personalKey
-      //'X-RapidAPI-Key': 'd9ebc6e747msh5fff7267e655227p111cdejsn580fb834d2d6',//tcetidKey
+      'X-RapidAPI-Key': '',//personalKey
+      //'X-RapidAPI-Key': '',//tcetidKey
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
